5df4f65ace4fe9d96ed92f409262c388ecb9ee00
nwright <nathanwrightbusiness@gmail.com>
2024-01-09 18:55:11 +0000

    Added error handler

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import niobot
 import logging
 import config
 
-logging.basicConfig(level=logging.INFO, filename="bot.log")
+logging.basicConfig(level=logging.DEBUG, filename="bot.log")
 
 bot = niobot.NioBot(
     homeserver=config.HOMESERVER,
     user_id=config.USER_ID,
-    device_id='bot-test',
+    device_id=config.DEVICE_ID,
     store_path='./store',
-    command_prefix="!",
+    command_prefix="!bot ",
     owner_id="@nate-wright:matrix.nwright.tech"
 )
 # We also want to load `fun.py`'s commands before starting:
@@ -24,6 +24,15 @@ async def on_ready(_):
 
 @bot.command()
 async def ping(ctx):  # can be invoked with "!ping"
-    await ctx.reply("Pong!")
+    await ctx.respond("Pong!")
 
-bot.run(password=config.PASSWORD)
+# And while we're at it, we can add an event listener.
+# For this example, we'll add an event listener that tells the user if there's a command error.
+@bot.on_event("command_error")
+async def on_command_error(ctx: niobot.Context, error: Exception):
+    """Called when a command raises an exception"""
+    # Take a look at the event reference for more information about events.
+    # Now, we can send a message to the user.
+    await ctx.respond("Error: {}".format(error))
+
+bot.run(access_token=config.ACCESS_TOKEN)
